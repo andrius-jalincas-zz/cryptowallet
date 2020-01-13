7fcda958057d56774e908130a9669a3c5e8e3afe
Andrius Jalincas <ajalincas@gmail.com>
2020-01-13 15:14:42 +0200

    swagger docs

--- src/Controller/AssetController.php
+++ src/Controller/AssetController.php
@@ -6,11 +6,13 @@ use App\Entity\Asset;
 use App\Entity\User;
 use App\Service\Asset as AssetService;
 use App\Service\Currency;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use  Symfony\Component\HttpFoundation\Response;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use OpenApi\Annotations as OA;
 
 /**
  * Class AssetController
@@ -22,8 +24,17 @@ class AssetController extends AbstractController
 {
     /**
      * @Route("/", name="asset.list", methods={"GET"})
+     * @return JsonResponse
+     * @OA\Get(
+     *     path="/api/asset",
+     *     summary="get user assets",
+     *     @OA\Response(
+     *         response=200,
+     *         description="Success"
+     *     )
+     * )
      */
-    public function list()
+    public function list(): JsonResponse
     {
         /** @var User $user */
         $user = $this->getUser();
@@ -49,8 +60,40 @@ class AssetController extends AbstractController
 
     /**
      * @Route("", name="asset.add", methods={"POST"})
+     * @param Request $request
+     * @param AssetService $assetService
+     *
+     * @return JsonResponse
+     * @OA\Post(
+     *     path="/api/asset",
+     *     summary="add new asset",
+     *     @OA\RequestBody(
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     *                 @OA\Property(
+     *                     property="label",
+     *                     type="string"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="amount",
+     *                     type="float"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="currency",
+     *                     type="string"
+     *                 ),
+     *                 example={"label": "BTC", "currency": "BTC", "amount": 0.1}
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Success"
+     *     )
+     * )
      */
-    public function add(Request $request, AssetService $assetService)
+    public function add(Request $request, AssetService $assetService): JsonResponse
     {
         $requestData = json_decode($request->getContent(), true);
 
@@ -76,8 +119,52 @@ class AssetController extends AbstractController
 
     /**
      * @Route("/{id}", name="asset.update", methods={"PUT"})
+     * @param Request $request
+     * @param int $id
+     * @param AssetService $assetService
+     *
+     * @return JsonResponse
+     *
+     * @OA\Put(
+     *     path="/api/asset/{id}",
+     *     summary="update asset",
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         description="ID asset",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="integer",
+     *             format="int8"
+     *         )
+     *     ),
+     *     @OA\RequestBody(
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     *                 @OA\Property(
+     *                     property="label",
+     *                     type="string"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="amount",
+     *                     type="float"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="currency",
+     *                     type="string"
+     *                 ),
+     *                 example={"label": "BTC", "currency": "BTC", "amount": 0.2}
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Success"
+     *     )
+     * )
      */
-    public function update(Request $request, int $id, AssetService $assetService)
+    public function update(Request $request, int $id, AssetService $assetService): JsonResponse
     {
         $requestData = json_decode($request->getContent(), true);
 
@@ -103,8 +190,32 @@ class AssetController extends AbstractController
 
     /**
      * @Route("/{id}", name="asset.remove", methods={"DELETE"})
+     * @param Request $request
+     * @param int $id
+     * @param AssetService $assetService
+     *
+     * @return JsonResponse
+     *
+     * @OA\Delete(
+     *     path="/api/asset/{id}",
+     *     summary="delete asset",
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         description="ID asset",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="integer",
+     *             format="int8"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Success"
+     *     )
+     * )
      */
-    public function remove(Request $request, int $id, AssetService $assetService)
+    public function remove(Request $request, int $id, AssetService $assetService): JsonResponse
     {
         /** @var User $user */
         $user = $this->getUser();
@@ -121,8 +232,21 @@ class AssetController extends AbstractController
     }
     /**
      * @Route("/balance", name="asset.balance", methods={"GET"})
+     * @param Request $request
+     * @param Currency $currencyService
+     *
+     * @return JsonResponse
+     *
+     * @OA\Get(
+     *     path="/api/asset/balance",
+     *     summary="get user assets with value in usd",
+     *     @OA\Response(
+     *         response=200,
+     *         description="Success"
+     *     )
+     * )
      */
-    public function balance(Request $request, Currency $currencyService)
+    public function balance(Request $request, Currency $currencyService): JsonResponse
     {
         /** @var User $user */
         $user = $this->getUser();
--- src/Controller/AuthenticationController.php
+++ src/Controller/AuthenticationController.php
@@ -5,16 +5,51 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Service\ApiToken;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use OpenApi\Annotations as OA;
 
 class AuthenticationController extends AbstractController
 {
     /**
      * @Route("/api/login", name="api.login", methods={"POST"})
+     * @param Request $request
+     * @param AuthenticationUtils $authenticationUtils
+     * @param ApiToken $apiTokenService
+     * @return JsonResponse
+     * @throws \Exception
+     * @OA\Post(
+     *     path="/api/login",
+     *     summary="Login",
+     *     @OA\RequestBody(
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     *                 @OA\Property(
+     *                     property="username",
+     *                     type="string"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="password",
+     *                     type="string"
+     *                 ),
+     *                 example={"username": "test", "password": "test"}
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Success",
+     *     )
+     * )
      */
-    public function apiLogin(Request $request, AuthenticationUtils $authenticationUtils, ApiToken $apiTokenService)
+    public function apiLogin(
+        Request $request,
+        AuthenticationUtils $authenticationUtils,
+        ApiToken $apiTokenService
+    ): JsonResponse
     {
         $errors = $authenticationUtils->getLastAuthenticationError();
         if ($errors) {
